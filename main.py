1b2e5e4a15c4fcf68d6feb8ad2ccd0e0307f4409
naren.gs <narensubbu1@gmail.com>
2025-02-23 16:24:27 +0530

    linted

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to odin!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8080)
