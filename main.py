200129554f3f666b2280e9112bb4e04f5f1efe8b
PaulAyushmaan <ayushmaanpaul2004@gmail.com>
2025-02-28 21:12:09 +0530

    PAN Verification API Integrated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.config import settings
 from app.core.database import connect_to_mongodb, close_mongodb_connection
 from app.api.endpoints import pan_validation
