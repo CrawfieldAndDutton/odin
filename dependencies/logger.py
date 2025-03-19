943e32a676c67c9b5a3ea62f46a492e2e9ce4d5d
PaulAyushmaan <ayushmaanpaul2004@gmail.com>
2025-03-19 19:29:00 +0530

    logger.py updated

--- dependencies/logger.py
+++ dependencies/logger.py
@@ -1,21 +1,16 @@
-# Standard library imports
-import sys
 import logging
+import sys
 
+logger = logging.getLogger('KYCFabricLogger')
+# iteratively removing default lambda log handlers
+for h in logger.handlers:
+    logger.removeHandler(h)
+
+FORMAT = '[%(levelname)s] %(message)s'
+h = logging.StreamHandler(sys.stdout)
+h.setFormatter(logging.Formatter(FORMAT))
 
 logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setFormatter(formatter)
-stream_handler.stream.reconfigure(encoding='utf-8')
-
-file_handler = logging.FileHandler('logs.log', encoding='utf-8')
-
-stream_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-
-logger.handlers = [stream_handler, file_handler]
-
+logger.addHandler(h)
 logger.setLevel(logging.INFO)
+logger.propagate = False
